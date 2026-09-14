a05719b106196f8c6492686240f0779496206b8e
Aditya (Ruben) Prasad <adityaprasad7123@gmail.com>
2026-09-14 08:15:32 -0400

    update: switched to lm studio

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 PORT = int(os.getenv("PORT", 8000))
 
 OLLAMA_HOST = os.getenv("OLLAMA_HOST")
+LMSTUDIO_HOST = os.getenv("LMSTUDIO_HOST")
 LLM_MODEL = os.getenv("LLM_MODEL")
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
 
