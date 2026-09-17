44fee868c0d74f13ec3a3b18f52579a32ef38907
Aditya (Ruben) Prasad <adityaprasad7123@gmail.com>
2026-09-17 03:08:32 -0400

    feat: concurrent search processing

--- backend/app/api/routes/search.py
+++ backend/app/api/routes/search.py
@@ -5,28 +5,33 @@ from app.schema.search import SearchRequest, SearchResult
 from app.services.embeddings import embed_text
 from app.services.search import dense_search_chunks, sparse_search
 
+import asyncio
+
 router = APIRouter(tags=["search"])
 
 
 @router.post("", response_model=list[SearchResult])
-def search(request: SearchRequest):
+async def search(request: SearchRequest):
     try:
         query_embedding = embed_text(request.query)
         chunks = get_all_embedded_chunks()
         chunks_unbed = get_all_chunks()
 
-        unbed_res = sparse_search(
-            query_raw=request.query,
-            chunks=chunks_unbed,
-            limit=request.limit,
-            repository_id=request.repository_id,
-        )
-
-        return dense_search_chunks(
-            query_embedding=query_embedding,
-            chunks=chunks,
-            limit=request.limit,
-            repository_id=request.repository_id,
+        sparse_res, dense_res = await asyncio.gather(
+            asyncio.to_thread(
+                sparse_search,
+                query_raw=request.query,
+                chunks=chunks_unbed,
+                limit=request.limit,
+                repository_id=request.repository_id,
+            ),
+            asyncio.to_thread(
+                dense_search_chunks,
+                query_embedding=query_embedding,
+                chunks=chunks,
+                limit=request.limit,
+                repository_id=request.repository_id,
+            ),
         )
 
         # return unbed_res
